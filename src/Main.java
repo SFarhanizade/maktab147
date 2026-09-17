d09198abae2961cbabf724a284a8e53fa10c5925
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2026-09-17 10:05:52 +0330

    add prints

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     static void main() {
         IO.println("Hello world!");
         IO.println("Hello maktab!");
+        IO.println("Hello maktab!");
+        IO.println("Hello maktab!");
+        IO.println("Hello maktab!");
+        IO.println("Hello maktab!");
+        IO.println("Hello maktab!");
     }
 }
